528e8fa806998b8444e968320e139e3aa518d7f6
ivantuanvuong <tuamphann5@gmail.com>
2024-09-05 16:03:07 +0700

    final subject

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String input = "hwfdawhwhcoomddfgwdc";
+        int result = CountTelex.countTelex(input);
+        System.out.println("Output: " + result);
+    }
+}
